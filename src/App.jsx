e2be4a8cfd2a20379c85191777e73d54973e8622
Ola Johannes Elvedahl <olajel@stud.ntnu.no>
2023-05-10 17:07:41 +0200

    new files

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import "./index.css";
+import logo from "./logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
+
+function App() {
+  return (
+    <>
+      <div id="content-container">
+        <div id="logo-container">
+          <img src={logo} alt="logo" id="logo" />
+          <h2 id="text">Her kommer det snart en ny nettside!</h2>
+        </div>
+        <div id="socials-container">
+          <p id="socials-text">
+            I mellomtiden kan du følge oss på sosiale medier
+          </p>
+          <a
+            href="vnd.instagram:https://www.instagram.com/bokstavmari/"
+            target="_blank">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              id="instagram"
+              className="icon"
+            />
+          </a>
+          <a
+            href="vnd.facebook:https://www.facebook.com/BokstavMari"
+            target="_blank">
+            <FontAwesomeIcon
+              icon={faFacebookF}
+              id="facebook"
+              className="icon"
+            />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
